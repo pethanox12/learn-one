5859eeb839ca646851f0140cf6a30da9082c6bc8
Adrian Talbot <adrian.r.talbot@gmail.com>
2023-04-15 10:58:09 +0100

    Added support for command line argument

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-    printf("Hello, world\n");
+    char    yourName[80];
+    
+    if (argc > 1) {
+        strcpy(yourName, argv[1]);
+    } else {
+        strcpy(yourName, "whatever your name is.");
+    }
+    
+    printf("Hello, %s\n", yourName);
     return 0;
 }
